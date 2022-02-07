d4eab5ae1deda2844cde3a0a770c08ad48e9779c
Veronika Mitichashvili <nika.mitichashili@gmail.com>
2022-02-07 18:05:05 +0400

    got the base ready

---
+++ twitter/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Twitter from "./components/twitter";
+
+function App() {
+  return (
+    <div className="App">
+      <Twitter />
+    </div>
+  );
+}
+
+export default App;
---
+++ twitter/src/components/twitter.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./twitter.scss";
+
+function Twitter() {
+  return <div className="container"></div>;
+}
+
+export default Twitter;
