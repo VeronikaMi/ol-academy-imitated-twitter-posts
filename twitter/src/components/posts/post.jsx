cc9b3af834c8a4d34c0a367c5b095f3d66968908
eniretake <ekzabakhidze@gmail.com>
2022-02-07 23:01:08 +0400

    added posts (got names and emails too)

--- twitter/src/components/posts/post.jsx
+++ twitter/src/components/posts/post.jsx
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./post.scss";
 
-function Post() {
+function Post(props) {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/users?id=${props.data.userId}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((user) => {
+        setUser(user[0]);
+      });
+  }, []);
+
   return (
     <div className="post">
       <div className="pic"></div>
       <div className="main">
         <div className="first-line">
           <div className="author-info">
-            <span className="name">Name</span>
-            <span className="mail">@mail</span>
+            <span className="name">{user.name}</span>
+            <span className="mail">{user.email}</span>
           </div>
           <div className="right">...</div>
         </div>
-        <div className="content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-          corrupti perspiciatis eveniet fuga dolore quia, assumenda consequuntur
-          ipsam rerum temporibus amet est architecto impedit cum aperiam
-          obcaecati vero nostrum maiores!
-        </div>
+        <div className="content">{props.data.body}</div>
         <div className="like">
           <svg>
             <path d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z"></path>
--- twitter/src/components/twitter.jsx
+++ twitter/src/components/twitter.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./twitter.scss";
-import Post from "./post";
+import Post from "./posts/post";
 
 function Twitter() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  }, []);
+
   return (
     <div className="container">
-      <Post />
+      {posts.length > 0 &&
+        posts.map((post) => <Post key={post.id} data={post} />)}
     </div>
   );
 }
